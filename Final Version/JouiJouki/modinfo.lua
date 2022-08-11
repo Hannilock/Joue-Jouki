77730cf9695f697334da42f725a6f9a130371654
JoJoeystar <joeywillian152@gmail.com>
2022-08-11 00:28:33 -0300

    Update modinfo.lua

--- Final Version/JouiJouki/modinfo.lua
+++ Final Version/JouiJouki/modinfo.lua
@@ -5,7 +5,7 @@ author = "Vih e Joey"
 version = "1.2.6" -- This is the version of the template. Change it to your own number.
 
 -- This is the URL name of the mod's thread on the forum; the part after the ? and before the first & in the url
-forumthread = "/files/file/950-extended-sample-character/"
+forumthread = "/"
 
 -- This lets other players know if your mod is out of date, update it to match the current version in the game
 api_version = 10
@@ -14,9 +14,10 @@ api_version = 10
 dst_compatible = true
 
 -- Not compatible with Don't Starve
-dont_starve_compatible = false
-reign_of_giants_compatible = false
-shipwrecked_compatible = false
+dont_starve_compatible = true
+reign_of_giants_compatible = true
+shipwrecked_compatible = true
+--client_only_mod = true
 
 -- Character mods are required by all clients
 all_clients_require_mod = true 
